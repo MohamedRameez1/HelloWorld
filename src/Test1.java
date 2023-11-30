63349e0f95638f6447bba85370e7f4bd79c27e30
Rameez111111 <rameezsi9311@gmail.com>
2023-11-30 10:18:43 +0530

    HelloWorld

---
+++ src/Test1.java
@@ -0,0 +1,18 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		{
+		int A= 30;
+		int B =0;
+		if(B!=0||A/B==30)
+		{
+			System.out.print("hELLO");
+		}
+		else {
+			System.out.print("world");
+		}
+	
+		}
+	}
+}
